d2d01a27373fc076cc040d53b2444603aa69a76b
Dynnikov Oleg <dinnikovoleg@gmail.com>
2018-09-21 18:45:26 +0300

    Initial UITableView project

---
+++ HW_22_UITableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW_22_UITableView
+//
+//  Created by Oleg Dynnikov on 9/21/18.
+//  Copyright © 2018 Oleg Dynnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
