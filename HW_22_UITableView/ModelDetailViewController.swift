a86d6be78d9e040e56c182482d010cb5f2c63b66
Dynnikov Oleg <dinnikovoleg@gmail.com>
2018-10-05 13:16:27 +0300

    Added minor changes in DetailVC

--- HW_22_UITableView/ModelDetailViewController.swift
+++ HW_22_UITableView/ModelDetailViewController.swift
@@ -13,12 +13,19 @@ class ModelDetailViewController: UIViewController {
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var modelNameLabel: UILabel!
     @IBOutlet private weak var descriptionLabel: UILabel!
+    @IBOutlet private weak var lcHeightImageView: NSLayoutConstraint!
 
+    private let navBarHeight: CGFloat = 64
     var armedUnit: ArmedUnit?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupData()
+    }
+
+    func setupData() {
         title = "Description"
+        lcHeightImageView.constant = view.bounds.height / 2 - navBarHeight
         imageView.image = armedUnit?.image ?? #imageLiteral(resourceName: "AFOU")
         modelNameLabel.text = armedUnit?.name ?? "No Data"
         descriptionLabel.text = armedUnit?.description ?? "No Data"
